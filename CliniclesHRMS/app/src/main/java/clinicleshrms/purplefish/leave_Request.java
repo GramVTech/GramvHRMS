bfeb7a3a05b10fe0f37d7dde88d3b158edbc0430
GramVTech <gramv.technologies@gmail.com>
2024-10-08 15:11:55 +0530

    First Commit

---
+++ CliniclesHRMS/app/src/main/java/clinicleshrms/purplefish/leave_Request.java
@@ -0,0 +1,23 @@
+package clinicleshrms.purplefish;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class leave_Request extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_leave_request);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.leave_request), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
